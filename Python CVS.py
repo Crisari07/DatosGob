eea3d8ce7cf1d86a4fd2d4041c66afb469a65d5e
Crisari07 <48942823+Crisari07@users.noreply.github.com>
2019-09-14 21:42:33 -0500

    Delete Python CVS.py

--- Python CVS.py
+++
@@ -1,8 +0,0 @@
-
-import csv
- 
-with open('1985Precip.csv') as File:
-    reader = csv.reader(File, delimiter=',', quotechar=',',
-                        quoting=csv.QUOTE_MINIMAL)
-    for row in reader:
-        print(row)    
